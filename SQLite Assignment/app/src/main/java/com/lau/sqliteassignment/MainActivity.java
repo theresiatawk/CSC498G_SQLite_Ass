d18ab8cf47a716833fc308dbb7698fb4cdae5442
Theresia Tawk <theresiatawk@hotmail.com>
2022-05-05 11:25:58 +0300

    Fetching from the db

--- SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
+++ SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.lau.sqliteassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -20,15 +21,31 @@ public class MainActivity extends AppCompatActivity {
         // Linking the variable the the list view layout
         list = (ListView) findViewById(R.id.list_view);
 
+        // Creating the array lists needed to store the info fetched from the database
+        ArrayList<String> exam_names = new ArrayList<>();
+        ArrayList<String> study_links = new ArrayList<>();
 
         // Creating db
         try{
             SQLiteDatabase sql_db = this.openOrCreateDatabase("examsdb", MODE_PRIVATE, null);
             sql_db.execSQL("CREATE TABLE if not exists exams(name VARCHAR, study_link VARCHAR)");
-            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Mobile Computing', 'https://ionicframework.com/')");
-            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Parallel Programming', 'https://docs.nvidia.com/cuda/')");
-            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Database Management', 'https://www.w3schools.com/sql/')");
-            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Object Oriented Programming', 'https://www.w3schools.com/java/')");
+//            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Mobile Computing', 'https://ionicframework.com/')");
+//            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Parallel Programming', 'https://docs.nvidia.com/cuda/')");
+//            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Database Management', 'https://www.w3schools.com/sql/')");
+//            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Object Oriented Programming', 'https://www.w3schools.com/java/')");
+
+            // Fetching from the db
+            Cursor c = sql_db.rawQuery("SELECT * FROM exams", null);
+            int exam_name_position = c.getColumnIndex("name");
+            int study_link_position = c.getColumnIndex("study_link");
+            c.moveToFirst();
+            int i = 0;
+            while (i < c.getCount()) {
+                exam_names.add(c.getString(exam_name_position));
+                study_links.add(c.getString(study_link_position));
+                c.moveToNext();
+                i++;
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
