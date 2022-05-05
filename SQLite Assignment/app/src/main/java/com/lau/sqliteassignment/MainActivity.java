42eb708e237b97d4bd0d4fed3c8d2bd32e4ad086
Theresia Tawk <theresiatawk@hotmail.com>
2022-05-05 11:18:07 +0300

    Creating the sqlite db

--- SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
+++ SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
@@ -2,8 +2,11 @@ package com.lau.sqliteassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Linking the variable the the list view layout
         list = (ListView) findViewById(R.id.list_view);
+
+
+        // Creating db
+        try{
+            SQLiteDatabase sql_db = this.openOrCreateDatabase("examsdb", MODE_PRIVATE, null);
+            sql_db.execSQL("CREATE TABLE if not exists exams(name VARCHAR, study_link VARCHAR)");
+            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Mobile Computing', 'https://ionicframework.com/')");
+            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Parallel Programming', 'https://docs.nvidia.com/cuda/')");
+            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Database Management', 'https://www.w3schools.com/sql/')");
+            sql_db.execSQL("INSERT INTO exams(name, study_link) VALUES ('Object Oriented Programming', 'https://www.w3schools.com/java/')");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
