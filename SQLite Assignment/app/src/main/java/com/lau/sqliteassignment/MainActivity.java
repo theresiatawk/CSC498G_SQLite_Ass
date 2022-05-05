930e900b112d7e9540ce6d4b07cc57b9ba4174e9
Theresia Tawk <theresiatawk@hotmail.com>
2022-05-05 10:32:39 +0300

    Creating the view needed

--- SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
+++ SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
@@ -3,12 +3,17 @@ package com.lau.sqliteassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.list_view);
+
     }
 }
