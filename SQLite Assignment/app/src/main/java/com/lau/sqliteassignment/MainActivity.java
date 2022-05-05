ad1375272f01a0a0401622d0773028e54c07f81f
Theresia Tawk <theresiatawk@hotmail.com>
2022-05-05 11:29:10 +0300

    Displaying the fetched exams on the screen

--- SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
+++ SQLite Assignment/app/src/main/java/com/lau/sqliteassignment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 c.moveToNext();
                 i++;
             }
+            ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, exam_names);
+            list.setAdapter(adapter);
         }
         catch (Exception e) {
             e.printStackTrace();
